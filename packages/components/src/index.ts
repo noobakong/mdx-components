502a94f5ce1c48d61fbbd7f54016e73006df610a
微笑 <991854471@qq.com>
2023-05-17 22:59:20 +0800

    feat: 新增Tweet、Badge、Image组件

---
+++ packages/components/src/index.ts
@@ -0,0 +1,9 @@
+import { Image } from "antd";
+import { Tweet } from "react-tweet";
+import { Badge } from "./Badge";
+
+export const components = {
+  Image,
+  Tweet,
+  Badge,
+};
---
+++ theme.config.jsx
@@ -0,0 +1,21 @@
+import {components} from 'mdx-elements';
+
+
+// eslint-disable-next-line import/no-anonymous-default-export
+export default {
+  logo: <span>MDX组件库</span>,
+  project: {
+    link: 'https://github.com/try-to-fly/mdx-components'
+  },
+  editLink: {
+    text: null,
+  },
+  feedback: {
+    content: null,
+  },
+  toc: {
+    float: true,
+    title: "目录",
+  },
+  components
+}
